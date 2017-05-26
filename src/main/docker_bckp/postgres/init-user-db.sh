91572a4f769776c7e7d7346547abb6e3104a3b82
Jechual <jechual@gmail.com>
2017-05-25 23:40:58 -0500

    cambios inesperados

--- src/main/docker_bckp/postgres/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER cacti PASSWORD 'cacti';
-    CREATE DATABASE cacti_data OWNER cacti;
-    GRANT ALL PRIVILEGES ON DATABASE cacti_data TO cacti;
-EOSQL
